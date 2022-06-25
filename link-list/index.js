2adbce9c6ab15d4e0d63fd6d95786c71836e4a74
cxYee <yechangxiaoycx@qq.com>
2022-06-25 23:47:12 +0800

    [feat]: 链表从尾部添加的实现

---
+++ link-list/index.js
@@ -0,0 +1,38 @@
+// 节点
+class LinkedNode {
+  constructor(value) {
+    this.value = value
+    // 存储下一个节点的应勇
+    this.next = null
+  }
+}
+
+// 链表
+class LinkedList {
+  constructor() {
+    this.count = 0
+    this.head = null
+  }
+  // 尾部添加节点
+  addAtTail(value) {
+    // 创建一个节点
+    const node = new LinkedNode(value)
+    // 判断链表是否空
+    if (this.count === 0) {
+      this.head = node
+    } else {
+      // 找到链表的尾部节点，将最后一个节点的next设置为node
+      let cur = this.head
+      while (cur.next !== null) {
+        cur = cur.next
+      }
+      cur.next = node
+    }
+    this.count++
+  }
+}
+
+const l = new LinkedList()
+l.addAtTail('1')
+l.addAtTail('2')
+l.addAtTail('3')
